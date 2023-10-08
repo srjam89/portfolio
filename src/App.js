7aae3f25b6067a4415c510485079ef30a91aa591
srjam89 <79203332+srjam89@users.noreply.github.com>
2023-10-08 12:50:39 +0100

    preloader added

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const App = () => {
           <Route path="/contactme" element={<ContactMe />} />
         </Routes>
       </div>
+      <div className="preloader"></div>
     </Router>
   );
 };
